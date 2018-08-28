b2b891da4c5c9b49dfc0de9901f4dff6f077517b
Hasan Jafri <hasan.jafri7@gmail.com>
2018-08-27 23:04:08 -0400

    add dg user model as well as mongodb client

---
+++ src/datagram_user_model.py
@@ -0,0 +1,11 @@
+from mongoengine import Document, StringField, BinaryField, DateTimeField
+import datetime
+
+class Datagram_User(Document):
+    business_email = StringField(required=True)
+    business_password = BinaryField(required=True)
+    business_name = StringField(required=True)
+    number = StringField(required=True)
+    dg_token = StringField(required=True)
+    date_created = DateTimeField(default=datetime.datetime.utcnow)
+    date_accessed = DateTimeField(default=datetime.datetime.utcnow)
